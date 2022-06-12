3dd3411bcb47ffc5c0c8a874e94f9192732c90e9
Vyvy-vi <vyom.j@protonmail.com>
2022-06-12 06:21:22 +0530

    chore: rename to assertPraiseAllowedInChannel

---
+++ packages/discord-bot/src/utils/assertPraiseAllowedInChannel.ts
@@ -0,0 +1,32 @@
+import { CommandInteraction, TextBasedChannel } from 'discord.js';
+import { settingValue } from 'api/dist/shared/settings';
+
+const getChannelId = (channel: TextBasedChannel): string => {
+  return channel.type === 'GUILD_PUBLIC_THREAD' ||
+    channel.type === 'GUILD_PRIVATE_THREAD' ||
+    channel.type === 'GUILD_NEWS_THREAD'
+    ? channel?.parent?.id || channel.id
+    : channel.id;
+};
+
+export const assertPraiseAllowedInChannel = async (
+  interaction: CommandInteraction
+): Promise<boolean> => {
+  const { channel } = interaction;
+  const allowedInAllChannels = (await settingValue(
+    'PRAISE_ALLOWED_IN_ALL_CHANNELS'
+  )) as boolean;
+  const allowedChannelsList = (await settingValue(
+    'PRAISE_ALLOWED_CHANNEL_IDS'
+  )) as number[];
+  if (
+    !channel ||
+    (!allowedInAllChannels &&
+      !allowedChannelsList.includes(Number.parseInt(getChannelId(channel))))
+  ) {
+    await interaction.editReply('Praise not allowed in this channel.');
+    return false;
+  } else {
+    return true;
+  }
+};
